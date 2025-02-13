049aa48def75856b092515877f022546151fd948
AmaanCD <amaan.shaikhs1810@gmail.com>
2025-02-14 02:12:22 +0530

    Keycloak Docker Image and config changes

--- src/main/java/com/project/ETour/security/SecurityConfig.java
+++ src/main/java/com/project/ETour/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.project.ETour.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
@@ -8,6 +9,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig  {
 
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity ) throws Exception {
         httpSecurity.
                 authorizeHttpRequests(auth->
